bd478ac25ed7bde68e2ac61a672811746c8c495b
Julio Henrique <julio.henrique2000@gmail.com>
2026-04-19 18:22:50 -0300

    feat: deleting eoutes/categoria.js

--- backend/routes/categorias.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Categorias funcionando!' });
-});
-
-module.exports = router;
